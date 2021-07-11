e1f032c5ea9641df09bc40d9ef525de444d7a71d
wonsukheo <62510983+wonsukheo@users.noreply.github.com>
2021-07-11 14:01:25 -0500

    recursive + depth fix

--- Assignment3/Assignment3/StepMaker.cs
+++ Assignment3/Assignment3/StepMaker.cs
@@ -25,32 +25,47 @@ namespace Assignment3
                 }
 
                 if (Math.Abs(newSteps[i] - newSteps[i - 1]) > 10)
-                {   
-                    if (i < recursive)
-                    {
-                        depth++;
-                    }
-
-                    MakeStepsRecursive(newSteps, i - 1, noise, depth);
-                    
-                    recursive += 4;
-                    i = 0;
+                {
+                    MakeStepsRecursive(newSteps, i - 1, noise, depth, recursive);
                 }
             }
 
             return newSteps;
         }
-        public static List<int> MakeStepsRecursive(List<int> newSteps, int startIndex, INoise noise, int depth)
+        public static List<int> MakeStepsRecursive(List<int> newSteps, int startIndex, INoise noise, int depth, int recursive)
         {
+            List<int> addedSteps = new List<int>(4);
+
             for (int i = 1; i < 5; i++)
             {
-                /*int result = i * (newSteps[startIndex + i] - newSteps[startIndex]) * 2;
-                result /= 10;
-                result += newSteps[startIndex] + noise.GetNext(depth);*/
-                int result = ((10 - i * 2) * newSteps[startIndex] + (i * 2) * newSteps[startIndex + i]) / 10;
+                int result = ((10 - i * 2) * newSteps[startIndex] + (i * 2) * newSteps[startIndex + 1]) / 10;
 
-                newSteps.Insert(startIndex + i, result + noise.GetNext(depth));
-            }                        
+                addedSteps.Add(result + noise.GetNext(depth));
+            }
+
+            newSteps.InsertRange(startIndex + 1, addedSteps);
+            recursive += 4;
+
+            for (int i = 0; i < newSteps.Count; i++)
+            {
+                if (i == 0)
+                {
+                    continue;
+                }
+
+                if (Math.Abs(newSteps[i] - newSteps[i - 1]) > 10)
+                {
+                    if (i < recursive)
+                    {
+                        MakeStepsRecursive(newSteps, i - 1, noise, depth + 1, recursive);
+                    }
+                    else
+                    {
+                        MakeStepsRecursive(newSteps, i - 1, noise, depth, recursive);
+                    }
+                }
+            }
+
             return newSteps;
         }
     }
